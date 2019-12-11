4ddec664972a7ad2af13e04de3f265ac30961b1e
Benjamin Small <ben20262@aim.com>
2019-12-11 01:33:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   final_key = nil
   final_value = 1000
-  name.each do |key, value|
+  name_hash.each do |key, value|
     if final_value > value
       final_value = value
       final_key = key
